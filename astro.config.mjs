77c4ede9b3de0838bdb19522a274d74704fcc9f7
Takashi Kojo <kojo@wolfssl.com>
2026-06-28 08:52:44 +0900

    Reference

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: 'https://kunisakikaiwai.github.io',
+  integrations: [mdx()],
 });
